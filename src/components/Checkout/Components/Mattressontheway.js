8298f6cc1dddda970a52a0ce57d41155a99788b9
Rabinswalterraj <58162262+Rabinswalterraj@users.noreply.github.com>
2022-12-26 17:16:25 +0530

    latest changes
    h

--- src/components/Checkout/Components/Mattressontheway.js
+++ src/components/Checkout/Components/Mattressontheway.js
@@ -7,20 +7,18 @@ import Navbar from '../../navbar/Navbar'
 function Mattressontheway() {
   return (
     <>
-    <Navbar/>
-    <div className='ontheway'>
-    <div>
-    <img src="assets/images/lastcheckout.png" alt="" className='imgontheway'/>
-    <h4 className='onthewaytxt'>Your Mattress Is On The Way</h4>
-    </div>
-    <Link to='/mattress'>
-    <button type="button" className='continuebtn'>Continue Shopping</button>  
-    </Link>
-    <button type='button' className='orderview'>View Orders</button>
-    </div>
-    <Footer/>
+      <div className='ontheway d-flex justify-content-center w-100'>
+        <div>
+          <img src="assets/images/lastcheckout.png" alt="" className='imgontheway' />
+          <h4 className='onthewaytxt'>Your Mattress Is On The Way</h4>
+        </div>
+        <Link to='/mattress'>
+          <button type="button" className='continuebtn'>Continue Shopping</button>
+        </Link>
+        <button type='button' className='orderview'>View Orders</button>
+      </div>
     </>
-    
+
   )
 }
 
